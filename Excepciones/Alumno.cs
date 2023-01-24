929c8c29f8f4a9d2549716d30246ef4d89f705af
Ariana Belgrado <belgradoariana@gmail.com>
2023-01-24 02:57:54 -0300

    Agregar archivos de proyecto.

---
+++ Excepciones/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class Alumno
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if (value.Trim().Length < 1 || value.Trim().Length > 50)
+                {
+                    throw new Exception("El nombre no puede estar vacio o superra los 50 caracteres.");
+                }
+
+                nombre = value.Trim();
+            }
+        }
+    }
+}
